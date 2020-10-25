550f519bb855b817b42b2cb51e53406d1eb626d8
Vanessa Sanchez <vanessa.sanchez023@mymdc.net>
2020-10-25 11:47:56 -0400

    First commit

---
+++ Assets/Scripts/Audio.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    public AudioClip musicClipOne;
+
+    public AudioClip musicClipTwo;
+
+    public AudioSource musicSource;
+
+    private int scoreValue = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+        musicSource.loop = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (scoreValue == 8)
+        {
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+            
+        }
+        
+        
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -10,24 +10,108 @@ public class PlayerScript : MonoBehaviour
     public float speed;
 
     public Text score;
+    
+    public Text winText;
+
+    public Text lives;
+    private bool facingRight = true;
+
+    private int livesValue = 3;
 
     private int scoreValue = 0;
+
+    public AudioClip musicClipOne;
+
+    public AudioClip musicClipTwo;
+
+    public AudioSource musicSource;
+
+    Animator anim;
+    
+    public bool isGrounded;
+    public Transform groundcheck;
+    public float checkRadius;
+    public LayerMask WhatIsGround;
 
     // Start is called before the first frame update
     void Start()
     {
+        
         rd2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         score.text = scoreValue.ToString();
+        winText.text = "";
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+        musicSource.loop = true;
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
+    
     {
+        isGrounded = Physics2D.OverlapCircle (groundcheck.position, checkRadius,WhatIsGround);
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
-    }
 
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
+
+         if (Input.GetKeyDown(KeyCode.A))
+        {
+
+          anim.SetInteger("State", 1);
+
+        }
+
+     if (Input.GetKeyUp(KeyCode.A))
+        {
+
+
+          anim.SetInteger("State", 0);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+
+          anim.SetInteger("State", 1);
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+
+
+          anim.SetInteger("State", 0);
+
+        }
+
+         if (Input.GetKeyDown(KeyCode.W))
+        {
+
+          anim.SetInteger("State", 2);
+
+         }
+
+     if (Input.GetKeyUp(KeyCode.W))
+        {
+
+          anim.SetInteger("State", 0);
+
+        }
+        
+    }
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
        if (collision.collider.tag == "Coin")
@@ -35,13 +119,45 @@ public class PlayerScript : MonoBehaviour
             scoreValue += 1;
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+
+            if (scoreValue == 4)
+            {
+                transform.position = new Vector3(41.0f, 173.0f, 0.0f);
+                livesValue = 3;
+                lives.text = livesValue.ToString();
+            }
+
+            if (scoreValue == 8)
+            {
+                winText.text = "You win! Game made by Vanessa Sanchez";
+                musicSource.clip = musicClipTwo;
+                musicSource.Play();
+            }
+
         }
+        
+        if (collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            lives.text = livesValue.ToString();
+            Destroy(collision.collider.gameObject);
+            
+            
+            
+            if(livesValue == 0)
+            {
+                winText.text = "You lose! By Vanessa Sanchez";
+                Destroy (this.gameObject);
+            }
+            
+        }    
+
 
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Ground")
+        if (collision.collider.tag == "Ground" && isGrounded)
         {
             if (Input.GetKey(KeyCode.W))
             {
@@ -53,5 +169,18 @@ public class PlayerScript : MonoBehaviour
             }
         }
     }
-    
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+
+    private void JumpAction()
+    {
+        rd2d.AddForce(transform.up * 600f);
+        anim.SetTrigger( "Jump" );
+    }
 }
